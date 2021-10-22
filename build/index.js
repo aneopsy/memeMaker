13873b93dd602619e46f14cdc2868559b810f95c
AneoPsy <paultevatheis@gmail.com>
2021-10-22 13:00:34 -1000

    Update

--- build/index.js
+++ build/index.js
@@ -35,19 +35,117 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var canvas_1 = require("canvas");
+var merge_images_1 = __importDefault(require("merge-images"));
 var express_1 = __importDefault(require("express"));
 var path_1 = __importDefault(require("path"));
 var dna_1 = require("./helpers/dna");
 var tables_1 = require("./helpers/tables");
 var app = (0, express_1.default)();
 var port = process.env.PORT || 8081;
+function createGif(file, fromFolder, toFolder, algorithm) {
+    if (algorithm === void 0) { algorithm = "neuquant"; }
+    return __awaiter(this, void 0, void 0, function () {
+        var _this = this;
+        return __generator(this, function (_a) {
+            return [2 /*return*/, new Promise(function (resolveMain) { return __awaiter(_this, void 0, void 0, function () {
+                    var _a, width, height, canvas2, ctx2, parts, img, dstPath, writeStream, encoder, canvas, ctx, _loop_1, i;
+                    return __generator(this, function (_b) {
+                        switch (_b.label) {
+                            case 0: return [4 /*yield*/, new Promise(function (resolve) {
+                                    var image = new canvas_1.Image();
+                                    image.onload = function () { return resolve([image.width, image.height]); };
+                                    image.src = path_1.default.join(fromFolder, file + ".png");
+                                })];
+                            case 1:
+                                _a = __read.apply(void 0, [_b.sent(), 2]), width = _a[0], height = _a[1];
+                                canvas2 = (0, canvas_1.createCanvas)(height, height);
+                                ctx2 = canvas2.getContext("2d");
+                                parts = [];
+                                img = new canvas_1.Image();
+                                img.onload = function () {
+                                    var w2 = img.width / 12;
+                                    for (var i = 0; i < 12; i++) {
+                                        var x = w2 * i;
+                                        canvas2.width = height;
+                                        canvas2.height = height;
+                                        ctx2.drawImage(img, x, 0, height, height, 0, 0, height, height);
+                                        parts.push(canvas2.toDataURL());
+                                    }
+                                };
+                                img.src = path_1.default.join(fromFolder, file + ".png");
+                                dstPath = path_1.default.join(toFolder, file + ".gif");
+                                writeStream = fs.createWriteStream(dstPath);
+                                writeStream.on("close", function () {
+                                    resolveMain();
+                                });
+                                encoder = new GIFEncoder(height, height, algorithm, true, 12);
+                                encoder.createReadStream().pipe(writeStream);
+                                encoder.start();
+                                encoder.setDelay(100);
+                                encoder.setQuality(30);
+                                canvas = (0, canvas_1.createCanvas)(height, height);
+                                ctx = canvas.getContext("2d");
+                                _loop_1 = function (i) {
+                                    return __generator(this, function (_c) {
+                                        switch (_c.label) {
+                                            case 0: return [4 /*yield*/, new Promise(function (resolve) {
+                                                    var image = new canvas_1.Image();
+                                                    image.onload = function () {
+                                                        ctx.drawImage(image, 0, 0);
+                                                        encoder.addFrame(ctx);
+                                                        resolve();
+                                                    };
+                                                    image.src = parts[i];
+                                                })];
+                                            case 1:
+                                                _c.sent();
+                                                return [2 /*return*/];
+                                        }
+                                    });
+                                };
+                                i = 0;
+                                _b.label = 2;
+                            case 2:
+                                if (!(i < 12)) return [3 /*break*/, 5];
+                                return [5 /*yield**/, _loop_1(i)];
+                            case 3:
+                                _b.sent();
+                                _b.label = 4;
+                            case 4:
+                                i++;
+                                return [3 /*break*/, 2];
+                            case 5:
+                                encoder.finish();
+                                return [2 /*return*/];
+                        }
+                    });
+                }); })];
+        });
+    });
+}
 app.get("/gif/:dna", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var params, dna, unsequenced, order, basePath, images, b64, headers;
+    var params, dna, unsequenced, order, basePath, images, b64, img, headers;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
@@ -55,28 +153,35 @@ app.get("/gif/:dna", function (req, res) { return __awaiter(void 0, void 0, void
                 dna = params === null || params === void 0 ? void 0 : params.dna;
                 unsequenced = (0, dna_1.unsequence)(dna);
                 order = [0, 1, 6, 5, 4, 3, 2];
-                basePath = path_1.default.join("./", "images/");
-                console.log(basePath);
+                basePath = path_1.default.join(__dirname, "../", "images/");
                 images = order.map(function (id) {
-                    return path_1.default.join(basePath, tables_1.attributeTable[id].name, tables_1.attributeTable[id][unsequenced[id]] + ".png");
+                    return path_1.default.join(basePath, tables_1.attributeTable[id].name, unsequenced[id].value + ".png");
                 });
-                console.log(JSON.stringify(images, null, 2));
-                return [4 /*yield*/, mergeImages(images, { Canvas: canvas_1.Canvas, Image: Image })];
+                return [4 /*yield*/, (0, merge_images_1.default)(images, { Canvas: canvas_1.Canvas, Image: canvas_1.Image })];
             case 1:
                 b64 = _a.sent();
-                console.log("Unsequenced Attributes: " + JSON.stringify(unsequenced, null, 2) + " - " + b64);
-                headers = { "Content-Type": "image/jpg" };
+                img = Buffer.from(b64.replace(/^data:image\/png;base64,/, ""), "base64");
+                headers = { "Content-Type": "image/png", "Content-Length": img.length };
                 res.writeHead(200, headers);
-                res.end(b64);
+                res.end(img);
                 return [2 /*return*/];
         }
+    });
+}); });
+app.get("/decode/:dna", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var params, dna, unsequenced, headers;
+    return __generator(this, function (_a) {
+        params = req.params;
+        dna = params === null || params === void 0 ? void 0 : params.dna;
+        unsequenced = (0, dna_1.unsequence)(dna);
+        headers = { "Content-Type": "application/json" };
+        res.writeHead(200, headers);
+        res.end(JSON.stringify(unsequenced));
+        return [2 /*return*/];
     });
 }); });
 app.get("/", function (req, res) { return res.send("You have reached the Pixsols Generator"); });
 app.listen(port, function () {
     console.log("Pixsols Generator listening at on port " + port);
 });
-function mergeImages(images, arg1) {
-    throw new Error("Function not implemented.");
-}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { Canvas, Image } from "canvas";
+import { Canvas, createCanvas, Image } from "canvas";
 import mergeImages from "merge-images";
+import GIFEncoder from "gif-encoder-2";
 import express from "express";
 import path from "path";
 
@@ -8,6 +9,56 @@ import { attributeTable } from "./helpers/tables";
 
 const app = express();
 const port = process.env.PORT || 8081;
+
+async function createGif(b64: string, algorithm: string = "neuquant") {
+  return new Promise<Buffer>(async (resolveMain) => {
+    let [width, height] = await new Promise((resolve) => {
+      const image = new Image();
+      image.onload = () => resolve([image.width, image.height]);
+      image.src = b64;
+    });
+
+    const canvas2 = createCanvas(height, height);
+    const ctx2 = canvas2.getContext("2d");
+    const parts: string[] = [];
+    const img = new Image();
+
+    img.onload = () => {
+      var w2 = img.width / 12;
+      for (var i = 0; i < 12; i++) {
+        var x = w2 * i;
+        canvas2.width = height;
+        canvas2.height = height;
+        ctx2.drawImage(img, x, 0, height, height, 0, 0, height, height);
+        parts.push(canvas2.toDataURL());
+      }
+    };
+
+    img.src = b64;
+
+    const encoder = new GIFEncoder(height, height, algorithm, true, 12);
+    encoder.start();
+    encoder.setDelay(100);
+    encoder.setQuality(30);
+
+    const canvas = createCanvas(height, height);
+    const ctx = canvas.getContext("2d");
+
+    for (let i = 0; i < 12; i++) {
+      await new Promise<void>((resolve) => {
+        const image = new Image();
+        image.onload = () => {
+          ctx.drawImage(image, 0, 0);
+          encoder.addFrame(ctx);
+          resolve();
+        };
+        image.src = parts[i];
+      });
+    }
+    encoder.finish();
+    resolveMain(encoder.out.getData());
+  });
+}
 
 app.get("/gif/:dna", async (req, res) => {
   const { params } = req;
@@ -21,11 +72,13 @@ app.get("/gif/:dna", async (req, res) => {
   );
 
   const b64 = await mergeImages(images, { Canvas: Canvas, Image: Image });
-  var img = Buffer.from(b64.replace(/^data:image\/png;base64,/, ""), "base64");
 
-  const headers = { "Content-Type": "image/png", "Content-Length": img.length };
+  const gif = await createGif(b64);
+  // var img = Buffer.from(b64.replace(/^data:image\/png;base64,/, ""), "base64");
+
+  const headers = { "Content-Type": "image/gif", "Content-Length": gif.length };
   res.writeHead(200, headers);
-  res.end(img);
+  res.end(gif);
 });
 
 app.get("/decode/:dna", async (req, res) => {
