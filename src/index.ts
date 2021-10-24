61d8afb981aa01da25cf2c91ff0456fb576deba0
AneoPsy <paultevatheis@gmail.com>
2021-10-24 13:24:41 -1000

    Update

--- src/index.ts
+++ src/index.ts
@@ -34,32 +34,7 @@ log.setLevel("info");
 
 require("dotenv").config();
 const app = express();
-var allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With"
-  );
-
-  // intercept OPTIONS method
-  if ("OPTIONS" == req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
-};
-
-app.configure(function () {
-  app.use(allowCrossDomain);
-  // app.use(express.bodyParser());
-  // app.use(express.methodOverride());
-  // app.use(app.router);
-  // app.use(express.static(path.join(application_root, "public")));
-  // app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  app.use(express.json());
-});
-
+app.use(express.json());
 const port = process.env.PORT || 8081;
 
 app.get("/gif/:dna", async (req, res) => {
